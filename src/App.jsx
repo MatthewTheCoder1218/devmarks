88bed40f24fd8df92f3cfd4882fb36b136b42bff
MatthewTheCoder1218 <matthewmicheal2023@gmail.com>
2025-04-18 08:20:47 -0700

    updated route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={
